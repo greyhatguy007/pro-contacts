0712a7aad4c681588c76fe539f31bf1097b3ced2
Ritvik <mritvikprasad@gmail.com>
2024-11-24 23:16:17 +0530

    created and containerized backend

---
+++ backend/main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type Contact struct{
+	ID int 	`json:"id"`
+	Name string `json:"name"`
+	Number string `json:"number"`
+	Email string `json:"email"`
+}
+
+func main(){
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err!=nil{
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS contacts (id SERIAL PRIMARY KEY, name TEXT, number TEXT, email TEXT)")
+
+	if err!=nil{
+		log.Fatal(err)
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/contacts", getContacts(db)).Methods("GET")
+	router.HandleFunc("/contacts/{id}", getContact(db)).Methods("GET")
+	router.HandleFunc("/contacts", createContact(db)).Methods("POST")
+	router.HandleFunc("/contacts/{id}", updateContact(db)).Methods("PUT")
+	router.HandleFunc("/contacts/{id}", deleteContact(db)).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", jsonContentTypeMiddleware(router)))
+}
+
+func jsonContentTypeMiddleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type","application/json")
+		handler.ServeHTTP(w,r)
+	})
+}
+
+func getContacts(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT * FROM contacts")
+		if err!=nil{
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		defer rows.Close()
+		contacts := []Contact{}
+		for rows.Next(){
+			var c Contact
+			if err:=rows.Scan(&c.ID, &c.Name, &c.Number, &c.Email); err!=nil{
+				log.Fatal(err)
+			}
+			contacts = append(contacts, c)
+		}
+		if err:=rows.Err(); err!=nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		json.NewEncoder(w).Encode(contacts)
+	}
+}
+
+func getContact(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+		var c Contact
+		err := db.QueryRow("SELECT * FROM contacts WHERE id = $1", id).Scan(&c.ID, &c.Name, &c.Number, &c.Email)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		json.NewEncoder(w).Encode(c)
+	}
+}
+
+func createContact(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var c Contact
+		json.NewDecoder(r.Body).Decode(&c)
+
+		err := db.QueryRow("INSERT INTO contacts (name, number, email) VALUES ($1, $2, $3) RETURNING id", c.Name, c.Number, c.Email).Scan(&c.ID)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		json.NewEncoder(w).Encode(c)
+	}
+}
+
+func updateContact(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var c Contact
+		json.NewDecoder(r.Body).Decode(&c)
+		vars := mux.Vars(r)
+		id := vars["id"]
+		_, err := db.Exec("UPDATE contacts SET name = $1, number = $2, email = $3 WHERE id = $4", c.Name, c.Number, c.Email, id)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		json.NewEncoder(w).Encode(c)
+	}
+}
+
+func deleteContact(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id := vars["id"]
+		var c Contact
+		err := db.QueryRow("SELECT * FROM contacts WHERE id = $1", id).Scan(&c.ID, &c.Name, &c.Number, &c.Email)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		} else {
+			_, err := db.Exec("DELETE FROM contacts WHERE id = $1", id)
+			if err != nil {
+				//todo : fix error handling
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+			json.NewEncoder(w).Encode("User deleted")
+		}
+	}
+}
