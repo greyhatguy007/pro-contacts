951989c4aaf437edbfe975f77f506a55bcd97090
Ritvik <mritvikprasad@gmail.com>
2024-11-25 12:23:03 +0530

    init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex flex-row justify-center h-screen items-center dark:bg-slate-800">
+      <div className="text-5xl dark:text-white font-semibold">test</div>
+    </div>
+  )
+}
+
+export default App
